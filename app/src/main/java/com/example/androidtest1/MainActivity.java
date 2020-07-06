f1d32c12d74a57ec0f1f92da87cecb4254677696
Mustard <2930305214@qq.com>
2020-07-06 23:29:40 +0800

    hw1

--- app/src/main/java/com/example/androidtest1/MainActivity.java
+++ app/src/main/java/com/example/androidtest1/MainActivity.java
@@ -10,6 +10,9 @@ import android.view.View;
 
 import android.widget.Button;
 
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -21,12 +24,32 @@ public class MainActivity extends AppCompatActivity {
 
         Button btn = findViewById(R.id.btn_click);
         final TextView tv=findViewById(R.id.tv_title);
+        final ImageView iv=findViewById(R.id.iv_background);
+        final ProgressBar pb=findViewById(R.id.pb_loading);
+        EditText et=findViewById(R.id.et_log);
 
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                tv.setText("Happy Birthday!!!");
-                Log.i("MainActivity","onClick:Happy Birthday!");
+                pb.setVisibility(View.VISIBLE);
+                int progress=pb.getProgress();
+                progress+=10;
+                pb.setProgress(progress);
+                float alpha=iv.getAlpha();
+                alpha+=0.2;
+                iv.setAlpha(alpha);
+                if(progress==pb.getMax()){
+                    pb.setVisibility(View.INVISIBLE);
+                    tv.setText("Happy Birthday!!!");
+                    Log.i("MainActivity","onClick:Happy birthday!");
+                }
+                if(progress>pb.getMax()) {
+                    progress=0;
+                    pb.setProgress(progress);
+                    alpha=0;
+                    iv.setAlpha(alpha);
+                }
+                Log.i("MainActivity","onClick:Progress");
             }
         });
     }
